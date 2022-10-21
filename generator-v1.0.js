17faced6d63f5ea5e7cc0d4fa5418e400eb72785
briantuckerdesign <111880851+briantuckerdesign@users.noreply.github.com>
2022-10-21 09:49:13 -0400

    var to let
    changed var to let

--- generator-v1.0.js
+++ generator-v1.0.js
@@ -5,31 +5,32 @@ const capturelist = document.querySelectorAll(".gen_capture");
 
 function genGenerate() {
     //Gets date (used in the file labels), formats it to MMDDYY
-    var genDate = new Date();
-    var genDD = String(genDate.getDate()).padStart(2, "0");
-    var genMM = String(genDate.getMonth() + 1).padStart(2, "0");
-    var genYYYY = genDate.getFullYear();
-    var genYY = genYYYY.toString().substr(-2);
+    let genDate = new Date();
+    let genDD = String(genDate.getDate()).padStart(2, "0");
+    let genMM = String(genDate.getMonth() + 1).padStart(2, "0");
+    let genYYYY = genDate.getFullYear();
+    let genYY = genYYYY.toString().substr(-2);
     genDate = genMM + genDD + genYY;
 
     //Defines scale of image export (@1x @2x etc) based on user input
-    var genScale = document.getElementById("gen_scale-id").value;
+    let genScale = document.getElementById("gen_scale-id").value;
 
     //Defines name folder of zip folder
-    var genFolderName = document.getElementById("gen_folder-name").value;
+    let genFolderName = document.getElementById("gen_folder-name").value;
+    //idk?
     document.querySelector("#gen_images").innerHTML = "";
 
     //Loops through capturelist and runs html2canvas to convert each div to a canvas
     for (let i = 0; i < capturelist.length + 1; i++) {
         html2canvas(capturelist[i], {
             //Sets scale based on input
-            scale: genScale
+            scale: genScale,
         }).then((canvas) => {
             //Labels each file using [.gen_slug] and genScale
-            var label = capturelist[i].querySelector(".gen_slug").innerHTML + "_" + genDate + "-@" + genScale + "x";
+            let label = capturelist[i].querySelector(".gen_slug").innerHTML + "_" + genDate + "-@" + genScale + "x";
 
-            var imgdata = canvas.toDataURL("image/png");
-            var obj = document.createElement("img");
+            let imgdata = canvas.toDataURL("image/png");
+            let obj = document.createElement("img");
             obj.src = imgdata;
             zip.file(
                 label + ".png",
@@ -42,7 +43,8 @@ function genGenerate() {
 
             //You should have a #gen_images div somewhere, it can be hidden. 
             $("#gen_images").append('<img src="' + obj.src + '"/>');
-            var v = document.querySelector("#gen_images").children.length;
+            //
+            let v = document.querySelector("#gen_images").children.length;
             console.log(v)
             if (v == document.querySelector("#gen_wrapper").children.length) {
                 zip
