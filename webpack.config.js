8f5bb91b4a8623318cbef935ce101b885552d882
brianmtucker <btucker@industrydive.com>
2023-11-09 15:42:24 -0500

    5.0.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,30 @@
+// Packages our files into compressed browser friendly JS or TXT files
+
 const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env"],
-                    },
-                },
-            },
-        ],
-    },
-    optimization: {
-        minimize: false,
-        minimizer: [new TerserPlugin()],
-    },
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimize: false,
+    minimizer: [new TerserPlugin()],
+  },
 };
